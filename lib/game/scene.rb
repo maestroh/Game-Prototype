9883950298a1ee751eca53ac2fbd6551f7cec4b1
unknown <Nael@.(none)>
2012-08-29 23:28:05 -0500

    finished scene refactor

--- lib/game/scene.rb
+++ lib/game/scene.rb
@@ -20,6 +20,14 @@ class Scene
     object
   end
   
+  def update(game_object_ID, action_ID)
+    object = get_object(game_object_ID)
+    object.update(action_ID) unless object.nil? do
+       raise ArgumentError, "Object not found"
+    end
+     
+  end
+  
   def draw
     @scene.each {|o| o.draw}
   end
--- test/unit/scene_test.rb
+++ test/unit/scene_test.rb
@@ -1,35 +1,85 @@
 require 'minitest/autorun'
 require 'gosu'
 require File.expand_path('../../../lib/game/scene.rb', __FILE__)
+require_relative 'Mock.rb'
 
 class Scene_Test < MiniTest::Unit::TestCase
   
   def setup
-    #mock objects
-    @mock1 = MiniTest::Mock::new
-    @mock1.expect :id, 1
-    @mock1.expect :draw, nil
-    @mock2 = MiniTest::Mock::new
-    @mock2.expect :id, 2
-    @mock2.expect :draw, nil
     
-    #add mock objects to scene
-    @scene = Scene.new
-    @scene.add_object(@mock1)
-    @scene.add_object(@mock2)
   end
   
   def test_that_scene_returns_object_ids
-    ids = @scene.object_ids
+    mock1 = MiniTest::Mock::new
+    mock1.expect :id, 1
+    mock1.expect :draw, nil
+    mock2 = MiniTest::Mock::new
+    mock2.expect :id, 2
+    mock2.expect :draw, nil
     
-    assert_includes ids, @mock1.id 
-    assert_includes ids, @mock2.id
+    scene = Scene.new
+    scene.add_object(mock1)
+    scene.add_object(mock2)
+    
+    ids = scene.object_ids
+    
+    assert_includes ids, mock1.id 
+    assert_includes ids, mock2.id
   end
   
   def test_that_scene_returns_object
-    object1 = @scene.get_object(@mock1.id)
+    mock1 = MiniTest::Mock::new
+    mock1.expect :id, 1
+    mock1.expect :draw, nil
+    mock2 = MiniTest::Mock::new
+    mock2.expect :id, 2
+    mock2.expect :draw, nil
     
-    assert_equal object1.id, @mock1.id
+    scene = Scene.new
+    scene.add_object(mock1)
+    scene.add_object(mock2)
+    
+    object1 = scene.get_object(mock1.id)
+    
+    assert_equal object1.id, mock1.id
+  end
+  
+  def test_that_draw_calls_all_object_draw
+    mock1 = MiniTest::Mock::new
+    mock1.expect :id, 1
+    mock1.expect :draw, nil
+    mock2 = MiniTest::Mock::new
+    mock2.expect :id, 2
+    mock2.expect :draw, nil
+    
+    scene = Scene.new
+    scene.add_object(mock1)
+    scene.add_object(mock2)
+    
+    scene.draw
+    
+    mock1.verify_method(:draw)
+    mock2.verify_method(:draw)
+  end
+  
+  def test_that_update_calls_action_object_update
+    mock1 = MiniTest::Mock::new
+    mock1.expect :id, 1
+    mock1.expect :draw, nil
+    mock1.expect :update, nil, ["run"]
+    mock1.expect :nil?, false
+    
+    mock2 = MiniTest::Mock::new
+    mock2.expect :id, 2
+    mock2.expect :draw, nil
+    
+    scene = Scene.new
+    scene.add_object(mock1)
+    scene.add_object(mock2)
+    
+    scene.update(1, "run")
+    
+    mock1.verify_method(:update)
   end
   
 end
